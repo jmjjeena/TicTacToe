9ca9d7ed021df2bfa1259cde1c4e377e08d3d172
Jeena Chemparathy <jeena@Jeenas-MacBook-Pro.local>
2019-05-15 14:42:27 -0700

    Create the board

---
+++ index.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt');
+
+var board = {
+  1: ' ',
+  2: ' ',
+  3: ' ',
+  4: ' ',
+  5: ' ',
+  6: ' ',
+  7: ' ',
+  8: ' ',
+  9: ' '
+};
+
+function markBoard(position, mark) {
+  board[position] = mark.toUpperCase();
+}
+
+function printBoard() {
+  console.log('\n' +
+      ' ' + board[1] + ' | ' + board[2] + ' | ' + board[3] + '\n' +
+      ' ---------\n' +
+      ' ' + board[4] + ' | ' + board[5] + ' | ' + board[6] + '\n' +
+      ' ---------\n' +
+      ' ' + board[7] + ' | ' + board[8] + ' | ' + board[9] + '\n');
+}
